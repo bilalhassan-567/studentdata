7cc77f6e8dde459bfc672c76422a02296f20e38c
bilalhassan-567 <mbilalhassan567@gmail.com>
2024-11-14 18:25:58 +0500

    Added login and middle ware

--- resources/views/studentdata/index.blade.php
+++ resources/views/studentdata/index.blade.php
@@ -1,5 +1,26 @@
 @extends('studentdata.frontend')
 
+<div class="custom-header shadow-lg">
+    <div class="dropdown">
+        <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ Auth::user()->name }}
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a></li>
+            <li>
+                <form method="POST" action="{{ route('logout') }}" style="display: inline;">
+                    @csrf
+                    <button type="submit" class="dropdown-item">Logout</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+
+
+
 @section('content')
 <div class="container">
     <div class="row">
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
-
-
-Route::resource('studentdata', CategoryController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('studentdata', CategoryController::class);
-Route::post('studentdata', [CategoryController::class, 'store'])->name('studentdata.store');
-Route::get('studentdata/create', [CategoryController::class, 'create'])->name('studentdata.create');
+// routes/web.php
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('studentdata', CategoryController::class);
+});
+
+require __DIR__.'/auth.php';
